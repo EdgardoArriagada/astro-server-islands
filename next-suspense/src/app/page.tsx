133c71f2275cca96b625d0e521e7f71a1db7575d
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-11-09 19:38:45 -0300

    remove header

--- next-suspense/src/app/page.tsx
+++ next-suspense/src/app/page.tsx
@@ -1,20 +1,19 @@
-import { Suspense } from "react";
+import { Suspense } from 'react'
 
 async function SlowComponent() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  await new Promise((resolve) => setTimeout(resolve, 5000))
 
-  return <div>SlowComponent</div>;
+  return <div>SlowComponent</div>
 }
 
 export default function Home() {
   return (
     <div>
-      <Header />
       <h2>Before SlowComponent</h2>
       <Suspense fallback={<div>Loading...</div>}>
         <SlowComponent />
       </Suspense>
       <h2>After SlowComponent</h2>
     </div>
-  );
+  )
 }
